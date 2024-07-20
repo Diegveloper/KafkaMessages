6be946d33a66e26ea864b9c109588de82f6f898f
Your Name <diegod9405@gmail.com>
2024-07-19 22:01:27 -0600

    added kafka functionality so far just producing

--- src/main/java/com/ozomahtli/messages/controllers/MessageController.java
+++ src/main/java/com/ozomahtli/messages/controllers/MessageController.java
@@ -1,13 +1,19 @@
 package com.ozomahtli.messages.controllers;
 
 import com.ozomahtli.messages.dto.MessageDto;
+import com.ozomahtli.messages.services.KafkaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/messages")
 public class MessageController {
+    @Autowired
+    private KafkaProducerService kfkSrv;
+
     @PostMapping
     public String receiveMessage(@RequestBody MessageDto msg){
+        kfkSrv.sendMessage(msg.getMessage());
         System.out.println(msg.getMessage());
         return "200";
     }
---
+++ src/main/java/com/ozomahtli/messages/services/KafkaProducerService.java
@@ -0,0 +1,15 @@
+package com.ozomahtli.messages.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerService {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    private static final String TOPIC = "messages";
+    public void sendMessage(String message){
+        kafkaTemplate.send(TOPIC, message);
+    }
+}
